1f62d9f114bcd90499266188d29c46959cb5d732
Wagner Passarella <wagner.passarella@tecnospeed.com.br>
2018-07-23 23:58:15 -0300

    Arquivo de array com For

---
+++ Curso-Golang/ArraySliceMap/ForRange/forrange.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	numeros := [...]int{1, 2, 3, 4, 5} //Compilador que conta
+
+	for i, numero := range numeros {
+		fmt.Printf("%d) %d\n", i+1, numero)
+	}
+
+	for _, num := range numeros {
+		fmt.Println(num)
+	}
+}
